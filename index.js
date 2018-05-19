ba86838b0ea9b22c2b5e7c6a85b3d72347c83ad8
karly.mareka@gmail.com <karly.mareka@gmail.com>
2018-05-19 18:18:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (`string`.toLowerCase() === `string`) { 
+  if (string.toLowerCase() === string) { 
     console.log("I can't hear you!")
   }
-  else if (`string`.toUpperCase() === `string`) {
+  else if (string.toUpperCase() === string) {
     console.log("YES INDEED!")
   }
-  else if (`string` === "I love you, Grandma.") {
+  else if (string === "I love you, Grandma.") {
     console.log("I love you, too.")
   }
 }
